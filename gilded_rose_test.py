2d750ca0eb3b7cbaf96c77b137a50d96178c9754
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-08-31 19:43:59 +0000

    Plugged in full approval test set.

--- gilded_rose_test.py
+++ gilded_rose_test.py
@@ -7,35 +7,21 @@ class GildedRoseTest(unittest.TestCase):
       items = [
         Item(name='+5 Dexterity Vest', sell_in=10, quality=20),
         Item(name='Aged Brie', sell_in=2, quality=0),
+        Item(name='Elixir of the Mongoose', sell_in=5, quality=7),
+        Item(name='Sulfuras, Hand of Ragnaros', sell_in=0, quality=80),
+        Item(name='Sulfuras, Hand of Ragnaros', sell_in=-1, quality=80),
+        Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=15, quality=20),
+        Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=10, quality=49),
+        Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=5, quality=49),
+        Item(name='Conjured Mana Cake', sell_in=3, quality=6),  # <-- :O
       ]
-      GildedRose(items).update_quality()
-      result = items
-      verify(result)
+
+      days = 30
+      for day in range(days + 1):
+        GildedRose(items).update_quality()      
+
+      verify(items)
 
 if __name__ == "__main__":
     unittest.main()
 
-# 
-# S O L U T I O N
-#
-# def main():
-#     items = [
-#         Item(name='+5 Dexterity Vest', sell_in=10, quality=20),
-#         Item(name='Aged Brie', sell_in=2, quality=0),
-#         Item(name='Elixir of the Mongoose', sell_in=5, quality=7),
-#         Item(name='Sulfuras, Hand of Ragnaros', sell_in=0, quality=80),
-#         Item(name='Sulfuras, Hand of Ragnaros', sell_in=-1, quality=80),
-#         Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=15, quality=20),
-#         Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=10, quality=49),
-#         Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=5, quality=49),
-#         Item(name='Conjured Mana Cake', sell_in=3, quality=6),  # <-- :O
-#     ]
-
-#     days = 30
-#     for day in range(days+1):
-#         print('-------- day {} --------'.format(day))
-#         print('name, sellIn, quality')
-#         for item in items:
-#             print(item)
-#         print('')
-#         GildedRose(items).update_quality()
