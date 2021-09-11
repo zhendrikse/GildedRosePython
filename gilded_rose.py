8ab61cf305abe0ddb0d878514d73fa5eb91fd552
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-09-11 21:47:53 +0000

    POLYMORPHISM breakpoint

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,37 +1,38 @@
 class GildedRose:
-
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
+    def update_item_quality(self, item):
+        if item.name == "Aged Brie":
+            if item.quality < 50:
+                item.quality = item.quality + 1
+            item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
                 if item.quality < 50:
                     item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
+        elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+            if item.quality < 50:
+                item.quality = item.quality + 1
+                if item.sell_in < 11:
                     if item.quality < 50:
                         item.quality = item.quality + 1
+                if item.sell_in < 6:
+                    if item.quality < 50:
+                        item.quality = item.quality + 1
+
+            item.sell_in = item.sell_in - 1
+            if item.sell_in < 0:
+                item.quality = item.quality - item.quality
+        elif item.name == "Sulfuras, Hand of Ragnaros":
+            pass
+        else:
+            if item.quality > 0:
+                item.quality = item.quality - 1
+            item.sell_in = item.sell_in - 1
+
+    def update_quality(self):
+        for item in self.items:
+            self.update_item_quality(item)
 
 
 class Item:
