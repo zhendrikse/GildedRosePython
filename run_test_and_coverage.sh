d32566c5303730caed76a18971aa8f7558de411c
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-09-20 07:25:58 +0000

    Removed test file from coverage report

--- run_test_and_coverage.sh
+++ run_test_and_coverage.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-pytest && coverage run --branch --source gilded_rose,gilded_rose_test gilded_rose_test.py && coverage report -m && coverage html
+pytest && coverage run --branch --source gilded_rose gilded_rose_test.py && coverage report -m && coverage html
