0d4fd7cba3001b08284406cf63831458fabf85e4
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-09-29 17:54:36 +0000

    Approval test with combinations

--- gilded_rose_test.py
+++ gilded_rose_test.py
@@ -1,29 +1,25 @@
 import unittest
-from approvaltests.approvals import verify
-from gilded_rose import Item, GildedRose
+from approvaltests.combination_approvals import verify_all_combinations
+from gilded_rose import GildedRose, Item
+
 
 class GildedRoseTest(unittest.TestCase):
-  def test_main(self):
-      items = [
-        Item(name='+5 Dexterity Vest', sell_in=10, quality=20),
-        Item(name='Aged Brie', sell_in=2, quality=0),
-        Item(name='Elixir of the Mongoose', sell_in=5, quality=7),
-        Item(name='Sulfuras, Hand of Ragnaros', sell_in=0, quality=80),
-        Item(name='Sulfuras, Hand of Ragnaros', sell_in=-1, quality=80),
-        Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=15, quality=20),
-        Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=10, quality=49),
-        Item(name='Backstage passes to a TAFKAL80ETC concert', sell_in=5, quality=49),
-        Item(name='Conjured Mana Cake', sell_in=3, quality=6),  # <-- :O
-      ]
+    def test_add_combinatorial(self):
+        names = [
+            "foo", "Aged Brie", "Sulfuras, Hand of Ragnaros",
+            "Backstage passes to a TAFKAL80ETC concert"
+        ]
+        sellIns = [-1, 2, 6, 0, 11, 7]
+        qualities = [0, 48, 49, 50, 47]
+        verify_all_combinations(self.do_update_quality,
+                                [names, sellIns, qualities])
 
-      days = 30
-      for day in range(days + 1):
-        GildedRose(items).update_quality()      
+    def do_update_quality(self, name: str, sellIn: int, quality: int) -> str:
+        items = [Item(name, sellIn, quality)]
+        app = GildedRose(items)
+        app.update_quality()
+        return app.items[0]
 
-      #GildedRose(items).update_quality()
-      
-      verify(items)
 
 if __name__ == "__main__":
     unittest.main()
-
