a60a595f75b5f76d2e099878bb4351587cdef5d1
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-09-30 09:50:15 +0000

    Refacorted tests

--- gilded_rose_spec.py
+++ gilded_rose_spec.py
@@ -8,36 +8,29 @@ def do_update(item: Item):
 
 with description(GildedRose) as self:
 
-  with description("Given a generic item"):
-    with context("with quality and sell in 0"):
-      with before.each:
-          self.item = Item("Foo", 0, 0)
-          do_update(self.item)
-        
-      with context("when updating it"):
-        with it("should have decreased the sell by 1"):
-          expect(self.item.sell_in).to(equal(-1))
-        with it("should have left the quality unchanged"):
-          expect(self.item.quality.value).to(equal(0))
-  
-    with context("with quality and sell in 1"):
-      with before.each:
-          self.item = Item("Foo", 1, 1)
-          do_update(self.item)
-        
-      with context("when updating it"):
-        with it("should have decreased the sell by 1"):
-          expect(self.item.sell_in).to(equal(0))
-        with it("should have decreased the quality by 1"):
-          expect(self.item.quality.value).to(equal(0))
+  with description("Given a sulfuras item"):
+    with description("with quality and sell in 1"):
+      with description("when updating it"):
+        with it("should have left sell in and quality unchanged"):
+          item = SulfurasItem(1, 1)
+          do_update(item)
+          expect(item.sell_in).to(equal(1))
+          expect(item.quality.value).to(equal(1))
 
-  with context("Given a Sulfuras item"):
-    with before.each:
-        self.item = SulfurasItem(11, 47)
-        do_update(self.item)
-      
-    with context("when updating it"):
-      with it("should have left the sell in unchanged"):
-        expect(self.item.sell_in).to(equal(11))
-      with it("should have left the quality unchanged"):
-        expect(self.item.quality.value).to(equal(47))
+  with description("Given a generic item"):
+    with description("with quality and sell in 0"):
+      with description("when updating it"):
+        with it("should have decremented sell in and left quality unchanged"):
+          item = Item("Foo", 0, 0)
+          do_update(item)
+          expect(item.sell_in).to(equal(-1))
+          expect(item.quality.value).to(equal(0))
+  
+    with description("with quality and sell in 1"):
+      with description("when updating it"):
+        with it("should have decremented sell in and quality"):
+          item = Item("Foo", 1, 1)
+          do_update(item)
+          expect(item.sell_in).to(equal(0))
+          expect(item.quality.value).to(equal(0))
+
