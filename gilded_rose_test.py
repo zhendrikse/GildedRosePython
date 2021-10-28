286756fab8260a8d14479f9a91de6dfae4b12300
Zeger Hendrikse <zegerh@yahoo.co.uk>
2021-10-28 05:21:05 +0000

    Start of kata

--- gilded_rose_test.py
+++ gilded_rose_test.py
@@ -2,15 +2,13 @@ import unittest
 from approvaltests.combination_approvals import verify_all_combinations
 from gilded_rose import GildedRose, Item
 
-
 class GildedRoseTest(unittest.TestCase):
     def test_add_combinatorial(self):
         names = [
-            "foo", "Aged Brie", "Sulfuras, Hand of Ragnaros",
-            "Backstage passes to a TAFKAL80ETC concert"
+            "foo"
         ]
-        sellIns = [-1, 2, 6, 0, 11, 7]
-        qualities = [0, 48, 49, 50, 47]
+        sellIns = [0]
+        qualities = [0]
         verify_all_combinations(self.do_update_quality,
                                 [names, sellIns, qualities])
 
